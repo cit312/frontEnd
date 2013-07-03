f3e114a53e5c2cfec037a962063d244b0c504618
Brayden <braydenrhancock@gmail.com>
2013-07-03 15:11:45 -0700

    trynna get this to work

--- src/com/example/myfirstapp/CommBean.java
+++
@@ -1,9 +0,0 @@
-package com.example.myfirstapp;
-import java.io.Serializable;
-
-
-public class CommBean implements Serializable{
-	public Integer id = 2;
-    public String command = "daCommand";
-    public String pword = "pword";
-}
--- src/com/example/myfirstapp/Handler.java
+++
@@ -1,7 +0,0 @@
-package com.example.myfirstapp;
-
-import java.util.HashMap;
-
-public interface Handler {
-	public void handleIt(HashMap<String, String> data);
-}
